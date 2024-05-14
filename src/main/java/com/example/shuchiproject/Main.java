406ff68f1a425bf7362e34b6a611e4f98c929ae4
platterss <78766794+platterss@users.noreply.github.com>
2024-05-14 10:49:28 -0700

    Made window and elements scalable

--- src/main/java/com/example/shuchiproject/Main.java
+++ src/main/java/com/example/shuchiproject/Main.java
@@ -2,8 +2,10 @@ package com.example.shuchiproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,9 +16,22 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/main-view.fxml"));
-        Parent root = loader.load();
 
-        stage.setScene(new Scene(root));
+        Region region = loader.load();
+
+        double origW = 600;
+        double origH = 400;
+
+        Group group = new Group(region);
+        StackPane rootPane = new StackPane();
+        rootPane.getChildren().add( group );
+
+        Scene scene = new Scene(rootPane, origW, origH);
+
+        group.scaleXProperty().bind(scene.widthProperty().divide(origW));
+        group.scaleYProperty().bind(scene.heightProperty().divide(origH));
+
+        stage.setScene(scene);
         stage.show();
     }
 }
